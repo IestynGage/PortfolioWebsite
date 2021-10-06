2bab80fddf602d038df53e98fcc3a52bab877f70
Iestyn Gage <Iestyn.Gage@talkout.com>
2021-10-06 20:34:24 +0100

    Add fake Image

--- src/Components/BusinessCard.tsx
+++ src/Components/BusinessCard.tsx
@@ -9,9 +9,18 @@ function BusinessCard() {
     const number = '07533 122422';
 
     return (
-    <div style={{position:'absolute', left:'50%', top:'50%', transform:'translate(-50%, -50%)', padding:'5px', borderStyle: 'solid', borderRadius:'10px'}}>
-        <h1> {name} </h1>
+    <div style={{position:'absolute', left:'50%', top:'50%', transform:'translate(-50%, -50%)', padding:'15px', borderStyle: 'solid', borderRadius:'10px'}}>
+        <span style={{display:'flex', flexDirection:'row'}}>
+          <span style={{height:'50px', width:'50px', borderRadius:'50%',  backgroundColor: '#bbb', padding:'10px'}} />
+          <h1 style={{padding:'0 10px'}}> {name} </h1>
+        </span>
         <h2> {currentRole} at {currentCompany}</h2>
+        <span style={{display:'flex', flexDirection:'row'}}>
+          <button> Email </button> 
+          <button> LinkIn </button> 
+          <button> Github </button>
+          <button> Phone</button>
+        </span>
     </div>
   );
 }
