06be2f944651c416b971998f17304a512ccd31fc
Iestyn Gage <Iestyn.Gage@talkout.com>
2022-01-31 23:07:26 +0000

    Try AppBAr

--- src/Components/BusinessCard.tsx
+++ src/Components/BusinessCard.tsx
@@ -1,31 +1,25 @@
-import { Avatar, Paper } from '@mui/material';
-import { deepOrange } from '@mui/material/colors';
+import { AppBar } from '@mui/material';
 import ButtonLink from './ButtonLink';
 
 function BusinessCard() {
     const name = 'Iestyn Gage';
     const currentRole = 'Junior Software Engineer';
     const currentCompany = 'CoreFiling';
-  
-    const email = 'iestyngage@gmail.com';
-    const number = '07533 122422';
 
     return (
-        <div style={{position: 'absolute', top: "50%",left:'50%', msTransform:"translate(-50%, -50%)",  transform: "translate(-50%, -50%)", display: 'flex', flexDirection:'column', alignItems:'center', justifyContent: 'center'}}> 
-          <Avatar sx={{ bgcolor: deepOrange[500], width: 102, height: 102 }} style={{ margin:'15px'}} >I</Avatar>
-          <Paper variant="outlined"  elevation={3} style={{ padding:'15px'}}>
-              <div style={{display: 'flex', flexDirection:'column', alignItems:'center' }}> 
-                <h1 style={{color:'black', fontSize:'25px', margin:"0px", fontFamily: 'Arial, Helvetica, sans-serif'}}> {name} </h1>
-                <h2 style={{color:'#969a9f', fontSize:'14px', fontFamily: 'Arial, Helvetica, sans-serif'}}> {currentRole} at {currentCompany}</h2>
-              </div>
-              <span style={{display:'flex', flexDirection:'row'}}>
-                <ButtonLink href={''} label={'About me'} />
-                <ButtonLink newTab href={'https://github.com/IestynGage'} label={'GitHub'} />  
-                <ButtonLink newTab href={'https://www.linkedin.com/in/iestyngage/'} label={'linkedIn'} />
-                <ButtonLink newTab href={'mailto:iestyngage@gmail.com'} label={'Email'} />
-              </span>
-          </Paper>
-      </div>
+
+      <AppBar position="static">
+          <span style={{display:'flex', flexDirection:'row'}}>
+        <h1 style={{color:'black', fontSize:'25px', margin:"0px", fontFamily: 'Arial, Helvetica, sans-serif'}}> {name} </h1>
+        <h2 style={{color:'#969a9f', fontSize:'14px', fontFamily: 'Arial, Helvetica, sans-serif'}}> {currentRole} at {currentCompany}</h2>
+            <ButtonLink href={''} label={'About me'} />
+            <ButtonLink newTab href={'https://github.com/IestynGage'} label={'GitHub'} />  
+            <ButtonLink newTab href={'https://www.linkedin.com/in/iestyngage/'} label={'linkedIn'} />
+            <ButtonLink newTab href={'mailto:iestyngage@gmail.com'} label={'Email'} />
+            <ButtonLink href={'cv.pdf'} label={'Download CV'} download={true} />
+          </span>
+      </AppBar>
+
   );
 }
 
--- src/Components/ButtonLink.tsx
+++ src/Components/ButtonLink.tsx
@@ -4,13 +4,14 @@ interface ButtonLinkProps {
   href: string;
   label: string,
   newTab?: boolean,
+  download?: boolean | undefined,
 }
 
-function ButtonLink({label, href, newTab = false}:ButtonLinkProps) {
+function ButtonLink({label, href, newTab = false, download}:ButtonLinkProps) {
 
     return (
     <Button variant="contained" style={{margin:'0 10px 0 0px'}}> 
-      <a href={href} style={{color:'white', textDecoration: 'none', fontWeight: 510}} target={newTab ? "_blank" : ""}>{label}</a> 
+      <a href={href} style={{color:'white', textDecoration: 'none', fontWeight: 510}} target={newTab ? "_blank" : ""} download={download}>{label}</a> 
     </Button>
   );
 }
